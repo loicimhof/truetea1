67155cff89a76d20c49c191340641eeb850e6cc8
loicimhof <imhof.loic04@gmail.com>
2022-10-05 16:58:10 +0200

    v2.3
    version 2:
    task 3

--- build/resources/main/insert/select.sql
+++ build/resources/main/insert/select.sql
@@ -24,3 +24,17 @@ select sum(op.amount), tt.name as category, t.name as tea from users u, orders o
 select tt.req_rank_weight from tea_type tt, users u where u.rank_id = tt.id and u.id = :principal;
 
 select r.req_age from rank r, users u where u.rank_id = r.id and u.id = :principal;
+
+
+-- v2.3 not used anymore
+select u.first_name, o.id, op.id, op.amount from users u, orders o, order_positions op where u.id = o.users_id and o.id = op.order_id;
+
+-- v2.3 not used anymore
+
+select u.first_name, sum(op.amount) as teas from users u, orders o, order_positions op where u.id = o.users_id and o.id = op.order_id group by u.first_name order by teas desc;
+
+-- v2.3 - Get user with most bought teas
+select u.first_name from users u, orders o, order_positions op where u.id = o.users_id and o.id = op.order_id group by u.first_name order by sum(op.amount) desc limit 1;
+
+
+select u.* from users u, orders o, order_positions op where u.id = o.users_id and o.id = op.order_id group by u.first_name, u.id, u.created_at, u.modified_at, birth_date, email, last_name, password, seeds, u.created_by, u.last_modified_by, rank_id order by sum(op.amount) desc limit 1;
--- src/main/java/com/example/jwt/domain/user/UserRepository.java
+++ src/main/java/com/example/jwt/domain/user/UserRepository.java
@@ -1,11 +1,19 @@
 package com.example.jwt.domain.user;
 
 import com.example.jwt.core.generic.ExtendedRepository;
+import com.example.jwt.domain.order.dto.OwnTea;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends ExtendedRepository<User> {
     Optional<User> findByEmail(String email);
+
+    @Query(value = "select u.* from users u, orders o, order_positions op where u.id = o.users_id and o.id = op.order_id group by u.first_name, u.id, u.created_at, u.modified_at, birth_date, email, last_name, password, seeds, u.created_by, u.last_modified_by, rank_id order by sum(op.amount) desc limit 1;\n", nativeQuery = true)
+    User getShoppingQueen();
 }
